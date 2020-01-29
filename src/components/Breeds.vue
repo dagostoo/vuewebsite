3357750cbcfbbde8613ae9b46bdca0227cd5d492
unknown <t.aguraiuja@gmail.com>
2020-01-29 22:37:43 +0200

    Location and breeds

---
+++ src/components/Breeds.vue
@@ -0,0 +1,50 @@
+<template>
+    <section id="breeds">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">About breeds</h2>
+            <h3 class="section-subheading text-muted">Little information about our breeds.</h3>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Cane Corso Italiano, Cane di Macellaio, Sicilian Branchiero, Italian Mastiff</h4>
+            <p class="text-muted">There is not a lot of available information on the Cane Corso; it is believed that the dog originated in Italy and that an ancestor of the breed was the Roman Molossian (known as the Canis Pugnax), the war dog of the Roman Army. The breed’s name comes from the Latin term “cohors” which means “protector” or “guardian”, appropriate for a dog whose original purpose was as a guard dog as well as a hunting dog. The breed nearly became extinct, however as it had a sizable gene pool and as efforts were made to restore the breed, many cross breeds and other breeds increased the gene pool even further, helping to enhance the health of the breed. With a weight of up to 110 pounds, dogs of the breed are medium to large in size. The Cane Corso is easily trained (considered to be one of the most trainable and responsive of the Mastiff family) and it is recommended that a dog receive training in obedience and socialization early. Dogs of this breed are usually calm and friendly toward its owners and good with children, though can be suspicious and sometimes aggressive when encountering strangers. The Cane Corso is athletic and will enjoy going on runs and hikes on a regular basis. Should safe shelter be available, dogs of this breed can be happy living outdoors or indoors.</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Boston Terriers</h4>
+            <p class="text-muted">The Boston terrier breed originated around 1875, when Robert C. Hopper of Boston, purchased from Edward Burnett a dog named Judge (known later as Hooper's Judge), which was of a Bull and Terrier type lineage. Hooper's Judge is directly related to the original Bull and Terrier breeds of the 19th and early 20th centuries. The American Kennel Club cites Hooper's Judge as the ancestor of all true modern Boston Terriers.[9]
+Judge weighed about 32 pounds (15 kg). Judge was bred to Burnett's Gyp (or Kate).  Gyp was a white bulldog-type female, owned by Mr. Edward Burnett, of Southboro, MA. She weighed about 20 pounds, was stocky and strong and had the typical blocky head now shown in Bostons. From this foundation of the breed, subsequent breeders refined the breed into what we know of it today. [10] Bred down in size from fighting dogs of the Bull and Terrier types, the Boston Terrier originally weighed up to 44 pounds (20 kg) (Old Boston Bulldogs).[2] The breed was first shown in Boston in 1870. By 1889 the breed had become sufficiently popular in Boston that fanciers formed the American Bull Terrier Club, the breed's nickname, "roundheads". Shortly after, at the suggestion of James Watson (a noted writer and authority), the club changed its name to the Boston Terrier Club and in 1893 it was admitted to membership in the American Kennel Club, thus making it the first US breed to be recognized.[9] It is one of a small number of breeds to have originated in the United States. The Boston Terrier was the first non-sporting dog breed in the US.
+In the early years, the color and markings were not very important. By the 20th century the breed's distinctive markings and color were written into the standard, becoming an essential feature. The Boston Terrier has lost most of its aggressive nature, preferring the company of humans, although some males will still challenge other dogs if they feel their territory is being invaded. Boston University's mascot is Rhett the Boston Terrier. The Boston Terrier is also the mascot of Wofford College in Spartanburg, S.C..</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Maine Coon</h4>
+            <p class="text-muted">The first mention of Maine Coon cats in a literary work was in 1861, in Frances Simpson's The Book of the Cat (1903). F.R. Pierce, who owned several Maine Coons, wrote a chapter about the breed. During the late 1860s, farmers located in Maine told stories about their cats and held the "Maine State Champion Coon Cat" contest at the local Skowhegan Fair.
+In 1895, a dozen Maine Coons were entered into a show in Boston. On 8 May 1895, the first North American cat show was hosted at Madison Square Garden in New York City. A female Maine Coon brown tabby, named Cosey, was entered into the show. Owned by Mrs. Fred Brown, Cosey won the silver collar and medal and was named Best in Show. The silver collar was purchased by the Cat Fanciers' Association (CFA) Foundation with the help of a donation from the National Capital Cat Show. The collar is housed at the CFA Central Office in the Jean Baker Rose Memorial Library.
+In the early 20th century, the Maine Coon's popularity began to decline with the introduction of other long-haired breeds, such as the Persian, which originated in the Middle East. The last recorded win by a Maine Coon in a national cat show for over 40 years was in 1911 at a show in Portland, Oregon. The breed was rarely seen after that. The decline was so severe that the breed was declared extinct in the 1950s, although this declaration was considered to be exaggerated and reported prematurely at the time. The Central Maine Cat Club (CMCC) was created in the early 1950s by Ethylin Whittemore, Alta Smith and Ruby Dyer in attempts to increase the popularity of the Maine Coon. For 11 years, the CMCC held cat shows and hosted exhibitions of photographs of the breed and is noted for creating the first written breed standards for the Maine Coon.
+The Maine Coon was denied provisional breed status—one of the three steps required for a breed not yet recognized by the CFA to be able to compete in championship competitions[11]—by the CFA three times, which led to the formation of the Maine Coon Cat Club in 1973. The breed was finally accepted by the CFA under provisional status on 1 May 1975, and was approved for championship status on 1 May 1976. The next couple of decades saw a rise in popularity of the Maine Coon, with championship victories and an increase in national rankings. In 1985, the state of Maine announced that the breed would be named the official state cat. Today the Maine Coon is the third most popular cat breed, according to the number of kittens registered with the CFA.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Breeds',
+  
+}
+</script>
---
+++ src/components/Location.vue
@@ -0,0 +1,97 @@
+<template>
+    <section class="bg-light" id="location">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">location</h2>
+            <h3 class="section-subheading text-muted">The best way to choose a puppy is to play with him.</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="location-member">
+              <img class="mx-auto rounded-circle" src="img/location/1.jpg" alt="">
+              <h4>Tallinn, Estonia</h4>
+              <p class="text-muted">Vabaduse pst 215-9, 10916</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-linkedin-in"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="location-member">
+              <img class="mx-auto rounded-circle" src="img/location/2.jpg" alt="">
+              <h4>Phone number</h4>
+              <p class="text-muted">3725518112</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-linkedin-in"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="location-member">
+              <img class="mx-auto rounded-circle" src="img/location/3.jpg" alt="">
+              <h4>E-mail</h4>
+              <p class="text-muted">tariet.aguraiuja@gmail.com</p>
+              <ul class="list-inline social-buttons">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-linkedin-in"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Location',
+}
+</script>
