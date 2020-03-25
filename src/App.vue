aaefd15a3c20f4b87a241194ba053f0ed0a910ea
unknown <t.aguraiuja@gmail.com>
2020-03-25 14:05:02 +0200

    clients

--- src/App.vue
+++ src/App.vue
@@ -25,32 +25,7 @@
 
 
     <!-- Clients -->
-    <section class="py-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-3 col-sm-6">
-            <a href="#">
-              <img class="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt="">
-            </a>
-          </div>
-          <div class="col-md-3 col-sm-6">
-            <a href="#">
-              <img class="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt="">
-            </a>
-          </div>
-          <div class="col-md-3 col-sm-6">
-            <a href="#">
-              <img class="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt="">
-            </a>
-          </div>
-          <div class="col-md-3 col-sm-6">
-            <a href="#">
-              <img class="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt="">
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Clients/>
 
     <!-- Contact -->
     <Contact/>
@@ -278,11 +253,13 @@ import Puppies from './components/Puppies'
 import Navigation from "./components/Navigation"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Clients from "./components/Clients";
 
 
 export default {
   name: 'app',
   components: {
+    Clients,
     Footer,
     Header,
     Dogs,
